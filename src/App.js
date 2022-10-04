b47ed70eb0dd0e447f0677d3ead5b570ad7b5f59
Reactongraph <jsdeveloper@ongraphmail.com>
2022-10-04 15:38:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
+
+const useCustom = () => {
+  const [value, setValue] = useState({ current: "", previous: "" });
+
+  function handleChange(val) {
+    setValue({ current: val, previous: value.current });
+  }
+  return [value, handleChange];
+};
 
 function App() {
+  const [value, handleChange] = useCustom();
+
   return (
-    <div></div>
+    <div>
+      <h1>Type below input box</h1>
+      <input
+        type="text"
+        style={{ minWidth: "50vw" }}
+        placeholder="Enter Text "
+        onChange={(e) => handleChange(e.target.value)}
+      />
+
+      <fieldset style={{ marginTop: "1rem" }}>
+        <p>
+          <b>previous value:</b>
+          {value?.previous}
+        </p>
+        <p>
+          <b>current value:</b>
+          {value?.current}
+        </p>
+      </fieldset>
+    </div>
   );
 }
 
